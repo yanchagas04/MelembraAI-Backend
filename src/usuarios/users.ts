034c885516a40bdb1bfa47e8cc6a851eef5244c5
Yan Chagas <yanchagas2004@gmail.com>
2025-04-15 14:50:58 -0300

    feat: Rota de criação de usuários feita

--- src/usuarios/users.ts
+++ src/usuarios/users.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { getUsers } from "./getUsers";
+import { postUsers } from "./postUsers";
 
 export const users = Router();
 
 users.get("/", (req, res) => getUsers(req, res));
+users.post("/", (req, res) => postUsers(req, res));
